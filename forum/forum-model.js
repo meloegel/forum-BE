2950ba7ad1e5f6a88551c0ea45fbd7128030c47b
meloegel <meloegel@gmail.com>
2020-12-14 13:43:11 -0500

    finished forum model

--- forum/forum-model.js
+++ forum/forum-model.js
@@ -22,9 +22,12 @@ function getTopic(id) {
         .first()
 }
 
-
-
-
+// Need to test this endpoint //
+function getAllTopicComments(id) {
+    return db('comments')
+        .where({ 'comments.topicId': id })
+        .join('topics', 'topics.id', 'comments.topicId').orderBy('comments.id')
+}
 
 function addTopic(topic) {
     return db('topics').insert(topic, 'id')
@@ -32,4 +35,29 @@ function addTopic(topic) {
 
 function addComment(comment) {
     return db('comment').insert(comment, 'id')
+}
+
+function updateTopic(changes, id) {
+    return db('topics')
+        .where({ id })
+        .updates(changes)
+}
+
+function deleteTopic(id) {
+    return db('topics')
+        .where({ id })
+        .del()
+}
+
+function deleteComment(id) {
+    return db('comments')
+        .where({ id })
+        .del()
+}
+
+// Need to test this endpoint //
+function getUserTopics(id) {
+    return db('topics')
+        .where({ 'topics.userId': id })
+        .join('users', 'users.id', 'topics.userId').orderBy('topics.id')
 }
