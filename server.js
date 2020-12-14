e3fc80f8c5860e4a786f1d103f907691aed8995f
meloegel <meloegel@gmail.com>
2020-12-14 17:37:54 -0500

    updated server page

--- server.js
+++ server.js
@@ -2,23 +2,21 @@ const express = require('express')
 const helmet = require('helmet')
 const cors = require('cors')
 
-
-
+const restrictedMiddleware = require('./auth/restricted-middleware');
 
 const server = express();
 
-
-
+const AuthRouter = require('./auth/auth-router');
+const UserRouter = require('./users/users-router');
+const ForumRouter = require('./forum/forum-router');
 
 server.use(helmet())
 server.use(cors())
 server.use(express.json())
 
-
-
-
-
-
+server.use('/api/auth', AuthRouter);
+server.use('/api/auth/users', UserRouter)
+server.use('/api/auth/forum', ForumRouter)
 
 server.get('/', (req, res) => {
     const message = process.env.MESSAGE
