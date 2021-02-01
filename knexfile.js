ac8fc0ab81358eabb5fc8d819e65d3aa734e8b86
meloegel <meloegel@gmail.com>
2021-01-31 22:11:39 -0500

    deployment fix attempt 23

--- knexfile.js
+++ knexfile.js
@@ -37,10 +37,6 @@ module.exports = {
   production: {
     client: 'pg',
     connection: pgConnection,
-    pool: {
-      min: 2,
-      max: 10
-    },
     migrations: {
       directory: './data/migrations'
     },
